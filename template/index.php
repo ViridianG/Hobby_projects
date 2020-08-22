7e3a38f8bb3aca1f77fde15cf571240bfc5727d6
Grant <viridiang93@gmail.com>
2020-08-21 17:58:14 -0700

    Add files via upload

---
+++ template/index.php
@@ -0,0 +1,25 @@
+<?php 
+include('includes/header.php')
+?>
+            <main>
+                <section>
+                    <div class="heading-secondary">
+                    <h2> </h2>
+                    </div>
+                </section>
+                
+
+                <section>
+                </section>
+
+                <section></section>
+                <?php 
+                include('includes/footer.php')
+?>           
+ </main>
+            
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="scripts.js"></script>
+
+    </body>
+</html>
